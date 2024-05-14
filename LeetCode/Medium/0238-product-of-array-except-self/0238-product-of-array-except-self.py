119df14e5b531a28431d441584e9fb2eef2b2ddb
JONGMIN SHIN <92903481+JongDo737@users.noreply.github.com>
2024-05-14 16:21:02 +0900

    Time: 282 ms (46.75%), Space: 19.1 MB (63.40%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        result = [1] * n
         
-        # 왼쪽 곱을 result 배열에 저장
-        left_product = 1
-        for i in range(n):
-            result[i] = left_product
-            left_product *= nums[i]
+        result = []
         
-        # 오른쪽 곱을 이용해 결과 배열 업데이트
-        right_product = 1
-        for i in range(n-1, -1, -1):
-            result[i] *= right_product
-            right_product *= nums[i]
+        p = 1
+        for i in range(len(nums)):
+            result.append(p)
+            p = p * nums[i]
         
+        p = 1
+        for i in range(len(nums)-1, -1, -1):
+            result[i] = result[i] * p
+            p = p * nums[i]
+            
+        # print(result)
         return result
