eff644c5267b257209e3b74a77f21dbb9b6c76f3
JONGMIN SHIN <92903481+JongDo737@users.noreply.github.com>
2026-04-23 00:06:08 +0900

    [level 1] Title: 폰켓몬, Time: 0.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,10 @@
 from collections import Counter
 def solution(nums):
     answer = 0
-    
-    dict = Counter(nums)
-    
-    types = list(dict.keys())
-    
-    if len(types) > (len(nums) / 2):
+    set1 = set(nums)
+    if len(set1) > (len(nums) / 2):
         return len(nums) / 2
     else:
-        return len(types)
+        return len(set1)
     
     
